c0a694e5ffcb46aff9eba8e9b68998dc31ead288
rifkibayuariy <email.rifkibayu@gmail.com>
2023-11-12 17:22:12 +0700

    Upload Files

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import ReactLogo from "./assets/rack-transparent-logo.png";
+
+function App() {
+    return (
+        <>
+            <main className="w-full h-screen flex justify-center items-center">
+                <div>
+                    <img className="sm:w-44 lg:w-72" src={ReactLogo} alt="Rack" />
+                    <h1 className="text-center text-zinc-500 mt-2">Under development</h1>
+                </div>
+            </main>
+        </>
+    );
+}
+
+export default App;
