439bc4323b8c0345dd15da0bf1c99ca334b6cb0a
rifkibayuariy <email.rifkibayu@gmail.com>
2023-11-12 17:31:41 +0700

    Fix Image Width

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
         <>
             <main className="w-full h-screen flex justify-center items-center">
                 <div>
-                    <img className="sm:w-44 lg:w-72" src={ReactLogo} alt="Rack" />
+                    <img className="w-40 lg:w-72" src={ReactLogo} alt="Rack" />
                     <h1 className="text-center text-zinc-500 mt-2">Under development</h1>
                 </div>
             </main>
